3a675e9d138c4d482f9a23a2769f2f1760172241
Manuel Cruz <manuelcruz@manuels-air.myfiosgateway.com>
2022-11-01 19:07:42 -0400

    Basic 01

---
+++ 01_basic/01_helloworld.dart
@@ -0,0 +1,9 @@
+
+// All dart aplication must have a main function, for this sample,
+// we gonna print a hello word
+// For run the exam use dart command in your terminal and the path file
+
+main() {
+  print("Hello world");
+}
+
